567ba323c2e4fb013fde03f9663fc63909db3a2b
ameneg <adriano.meneghini@gmail.com>
2026-08-22 13:18:03 -0300

    added text to textnodes

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -7,6 +7,7 @@ from inline_markdown import (
     extract_markdown_images,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
 )
 
 
@@ -260,6 +261,55 @@ class TestSplitNodesLink(unittest.TestCase):
 
     def test_empty_list(self):
         self.assertListEqual([], split_nodes_link([]))
+
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test_all_types(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
+        )
+
+    def test_plain_text(self):
+        nodes = text_to_textnodes("just some plain text")
+        self.assertListEqual([TextNode("just some plain text", TextType.TEXT)], nodes)
+
+    def test_bold_only(self):
+        nodes = text_to_textnodes("a **bold** word")
+        self.assertListEqual(
+            [
+                TextNode("a ", TextType.TEXT),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" word", TextType.TEXT),
+            ],
+            nodes,
+        )
+
+    def test_image_and_link(self):
+        nodes = text_to_textnodes(
+            "![img](https://a.com/1.png) and [link](https://boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                TextNode("img", TextType.IMAGE, "https://a.com/1.png"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
+        )
 
 
 if __name__ == "__main__":
